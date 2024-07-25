c38c7b82ad504b484ff00e5e9a6570ee708f8c89
Kairo Sales <kairomatheussb@gmail.com>
2024-07-25 20:47:04 -0300

    feat: creating the project structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        COMMIT INICIAL
+      </header>
+    </div>
+  );
+}
+
+export default App;
