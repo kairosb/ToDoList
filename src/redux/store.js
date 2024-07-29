e9cba117050ee429f4e84a50e30518bb2a91402a
Kairo Sales <kairomatheussb@gmail.com>
2024-07-29 04:23:40 -0300

    feat: improvements to the redux framework and adding a localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if(serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (error) {
+        return undefined;
+    }
+}
+
+const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 export const store = configureStore({
     reducer: {
         todos: todoReducer
-    }
-})
+    },
+    preloadedState: loadState()
+});
+
+store.subscribe(() => {
+    saveState(store.getState());
+});
+
+export default store;
